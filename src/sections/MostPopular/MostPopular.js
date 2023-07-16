d496294770f234bce9cdb6317dd5cd06d89a41ce
Yaz <guemanzii@gmail.com>
2023-07-16 19:09:10 +0200

    L'ajout de section

--- src/sections/MostPopular/MostPopular.js
+++ src/sections/MostPopular/MostPopular.js
@@ -1,15 +1,19 @@
 import './MostPopular.css'
-import {Card} from '../../components/index'
+import {Card, SectionHeader } from '../../components/index'
 import popular_01 from '../../assets/images/popular-01.jpg';
-
+import popular_02 from '../../assets/images/popular-02.jpg';
+import popular_03 from '../../assets/images/popular-03.jpg';
+import popular_04 from '../../assets/images/popular-04.jpg';
+import popular_05 from '../../assets/images/popular-05.jpg';
+import popular_06 from '../../assets/images/popular-06.jpg';
 
 const MostPopular = () => {
   return (
     <>
       <div className='section-wrapper'>
-        <div className='section-header'>
-            <h3>Most Popular</h3>
-        </div>
+        <SectionHeader>
+          Most Popular
+          </SectionHeader>
         <div className='most-popular-items'>
             {/* <div className='most-popular-item'>
                 <div className='card-wrapper'>
@@ -28,9 +32,11 @@ const MostPopular = () => {
                 </div>
             </div> */}
             <Card image={popular_01}  title="Fortnite" category="Sandbox" rate="3.8" download="2.3M" />
-            <Card image="popular_02" title="PubG" category="Stream-X" rate="4.8" download="1.1M"/>
-            <Card image="popular_03" title="Dota2" category="Legendary" rate="2.7" download="5.3M"/>
-            <Card image="popular_04" title="CS-Go" category="Battle S" rate="3.9" download="6.1M"/>
+            <Card image={popular_02} title="PubG" category="Stream-X" rate="4.8" download="1.1M"/>
+            <Card image={popular_03} title="Dota2" category="Legendary" rate="2.7" download="5.3M"/>
+            <Card image={popular_04} title="CS-Go" category="Battle S" rate="3.9" download="6.1M"/>
+            <Card image={popular_05} title="CS-Go" category="Battle S" rate="3.9" download="6.1M"/>
+            <Card image={popular_06} title="CS-Go" category="Battle S" rate="3.9" download="6.1M"/>
 
         </div>
 
